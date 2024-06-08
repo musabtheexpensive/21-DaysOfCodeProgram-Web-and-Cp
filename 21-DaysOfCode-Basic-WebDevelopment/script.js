fbeee1bade6d63ec3a8ba0f1d958f13c59f49b77
Musab Al Mahi <musabalmahi53@gmail.com>
2024-06-08 23:07:17 +0600

    learning and implementing basic dom concept

--- 21-DaysOfCode-Basic-WebDevelopment/script.js
+++ 21-DaysOfCode-Basic-WebDevelopment/script.js
@@ -1,8 +1,20 @@
 function handleContact(event) {
   event.preventDefault();
-  name=event.target.name.value;
-  email=event.target.email.value;
-  message=event.target.message.value;
-  console.log("contact form submitted");
-  console.log(name, email,message);
+  name = event.target.name.value;
+  email = event.target.email.value;
+  message = event.target.message.value;
+
+  const successContainer = document.getElementById("success_container");
+  const nameParagraph = document.createElement("p");
+  nameParagraph.innerHTML = `Your Name: ${name}`;
+
+  const emailParagraph = document.createElement("p");
+  emailParagraph.innerHTML = `Your Name: ${name}`;
+
+  const messageParagraph = document.createElement("p");
+  messageParagraph.innerHTML = `Your Name: ${name}`;
+
+  successContainer.appendChild(nameParagraph);
+  successContainer.appendChild(emailParagraph);
+  successContainer.appendChild(messageParagraph);
 }
